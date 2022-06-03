6c8c8b25b61cd4dc5ab1893a85be34b91043f955
Mahmood Garibov <mahmood.garibov@gmail.com>
2022-06-04 01:10:53 +0400

    Mime type helper class completed

---
+++ MimeTypeHelper/MimeTypeHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MimeTypeHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MimeTypeHelper.Application.
+        }
+    }
+}
